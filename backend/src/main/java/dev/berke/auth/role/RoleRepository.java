6ca8873f42d769806c367268a5f6ff1b721fb365
Berke Sayın <sayinberke34@gmail.com>
2024-09-12 15:21:18 +0300

    Implement user and role repositories

---
+++ backend/src/main/java/dev/berke/auth/role/RoleRepository.java
@@ -0,0 +1,10 @@
+package dev.berke.auth.role;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface RoleRepository extends JpaRepository<Role, Integer> {
+
+    Optional<Role> findRoleByName(String role);
+}
