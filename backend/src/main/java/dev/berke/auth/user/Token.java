fe48ed85605663002cd508617909dd6e73541f09
Berke Sayın <sayinberke34@gmail.com>
2024-09-12 15:39:33 +0300

    Create the token entity and repository

---
+++ backend/src/main/java/dev/berke/auth/user/Token.java
@@ -0,0 +1,31 @@
+package dev.berke.auth.user;
+
+import jakarta.persistence.*;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAt;
+
+    @ManyToOne
+    @JoinColumn(name = "userId", nullable = false)
+    private User user;
+}
